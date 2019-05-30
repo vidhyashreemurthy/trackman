fd86ad4722ea7286fc54f5d1061dec4e88c9a3f7
Vidhyashree Murthy <vidhyashreemurthy@gmail.com>
2019-05-30 23:05:31 +0200

    pretty printing dependencies

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ from node import node
 
 TABLES_DIR = './tables'
 
-def print_tables(sql):
+def get_dependent_tables(sql):
     stmt = sqlparse.parse(sql)[0]
+    name_pairs = []
     aliases = set()
-    print('=' * 5, stmt, '=' * 5)
     for token in stmt.tokens:
         if type(token) == sqlparse.sql.Identifier:
             alias = token.get_alias()
             parent_name = token.get_parent_name()
             real_name = token.get_real_name()
             if parent_name and parent_name not in aliases:
-                print(parent_name, real_name)
+                name_pairs.append((parent_name, real_name))
                 aliases.add(alias)
-        
+    return name_pairs
 
 def get_from_string(filename):
     with open(os.path.join(TABLES_DIR, filename)) as f:
@@ -28,15 +28,19 @@ def get_from_string(filename):
         from_str = L[0]['M']['from']['S']
     else:
         from_str = obj['query']['M']['from']['S']
-    return from_str
+    
+    return obj['schema']['S'], obj['table']['S'], from_str
 
 if __name__ == '__main__':
     miss = 0
     filenames = [filename for filename in os.listdir(TABLES_DIR) if filename.endswith('.json')]
     for filename in filenames:
         try:
-            from_str = get_from_string(filename)
-            print_tables(from_str)
+            schema, table, from_str = get_from_string(filename)
+            name_pairs = get_dependent_tables(from_str)
+            print('Schema: {}, Table: {}'.format(schema, table))
+            print('Dependencies:', name_pairs)
+            print()
         except:
             miss += 1
     
